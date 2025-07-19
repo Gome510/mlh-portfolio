e1a92d2e2fd425b5c8f5878281e54c0b819b3448
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-07-18 17:36:24 -0700

    Added test_app to test the portolio website to and check for errors. Adjusted __init__.py to account for edge cases where a user may not enter a name, email, or content.

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,11 @@ from playhouse.shortcuts import model_to_dict
 load_dotenv()
 app = Flask(__name__)
 
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
 
 print(mydb)
 
@@ -21,16 +25,24 @@ class TimelinePost (Model):
     class Meta:
         database = mydb
 
+
 mydb.connect()
 mydb.create_tables([TimelinePost])
 
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
-    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    name = request.form.get('name', '').strip()
+    email = request.form.get('email', '').strip()
+    content = request.form.get('content', '').strip()
 
+    if not name:
+        return "Invalid name", 400
+    if not email or "@" not in email:
+        return "Invalid email", 400
+    if not content:
+        return "Invalid content", 400
+
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
     return jsonify(model_to_dict(timeline_post))
 
 @app.route('/api/timeline_post', methods=['GET'])
