d05c9d89946f7e7c571b235676d61c3264b81d62
Pablo Gomez-Echegaray <pablogomezechegaray@gmail.com>
2025-07-26 23:39:05 -0700

    check redeploy

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,10 +2,5 @@ cd mlh-portfolio
 git fetch
 git reset origin/main --hard
 
-python3 -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-deactivate
-
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
